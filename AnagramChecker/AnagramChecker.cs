985b397470ec0283c362643cf499dd954f30c958
Elavarasi Veerappan <elavarasi.veerappan@gmail.com>
2024-02-24 19:42:32 +0000

    updated comments for each method and validation

--- AnagramChecker/AnagramChecker.cs
+++ AnagramChecker/AnagramChecker.cs
@@ -12,39 +12,31 @@ namespace AnagramChecker
         //non-static public method
         public bool WordsAreAnagrams(string word1, string word2)
         {
-
-        
-            string firstString = CleanString(word1);
-            string secondString = CleanString(word2);
+                    
+            string firstString = RemoveNonAlphabeticCharacters(word1);
+            string secondString = RemoveNonAlphabeticCharacters(word2);
 
             //Validation Check
             #region
+           
+            // Check if the words are identical
             if (firstString.Equals(secondString))
             {
+                // Two identitical word canot be anagrams
                 return false;
             }
-      
+    
             // Check if either word is empty
             if (string.IsNullOrEmpty(word1))
             {
                 throw new ArgumentException("First argument cannot be empty.");
             }
-
-            if (string.IsNullOrEmpty(word2))
+            else if (string.IsNullOrEmpty(word2))
             {
                 throw new ArgumentException("Second argument cannot be empty.");
             }
-
+                    
             // check if either word contain whitespaces
-            if (string.IsNullOrWhiteSpace(word1))
-            {
-                throw new ArgumentException("Input cannot be empty or contain only whitespaces.", nameof(word1));
-            }
-            else if (string.IsNullOrWhiteSpace(word2))
-            {
-                throw new ArgumentException("Input cannot be empty or contain only whitespaces.", nameof(word2));
-            }
-         
             if (word1.Contains(" ") || word2.Contains(" "))
             {
                 if (word1.Contains(" ") && word2.Contains(" "))
@@ -67,17 +59,22 @@ namespace AnagramChecker
             }
             #endregion
 
+            //Convert string to character array  
             char[] firstCharsArray = firstString.ToCharArray();
             char[] secondCharsArray = secondString.ToCharArray();
+
+            //Sort array 
             Array.Sort(firstCharsArray);
             Array.Sort(secondCharsArray);
 
+            //Check two character arrays are in the same order, the SequenceEqual method will return true, indicating that the two words are anagrams. 
             return firstCharsArray.SequenceEqual(secondCharsArray);
         }
 
-       
-        private string CleanString(string input)
+        // removes non-alphabetic characters and converts it to the lowercse
+        private string RemoveNonAlphabeticCharacters(string input)
         {
+            // check if either word contains null
             if (input == null)
             {
                 throw new ArgumentNullException();
